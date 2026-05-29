cef3fc536e82f701dd76facd5220dd094c000156
rpnchlr <121367757+rpnchlr@users.noreply.github.com>
2026-05-29 21:02:18 +0530

    added time flag to get user choice for waiting time.

--- booster.sh
+++ booster.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+wait=2
+
+while getopts "t:" opt; do
+  case $opt in
+  t)
+    wait="$OPTARG"
+    ;;
+  \?)
+    echo "Invalid options"
+    exit 1
+    ;;
+  esac
+done
+
 count=1
 while [[ true ]]; do
+  sleep "$wait"
   git add .
-  git commit -m "initial commit $count"
+  git commit -m "Commit no. $count"
   ((count++))
 done
